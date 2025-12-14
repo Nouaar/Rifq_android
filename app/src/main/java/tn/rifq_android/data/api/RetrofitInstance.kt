9066446b8d752fea97eaa69efe87fca9e4295319
Maamar NourAllah <nourallah.maamar@esprit.tn>
2025-12-15 00:08:13 +0100

    stripe fixed

--- app/src/main/java/tn/rifq_android/data/api/RetrofitInstance.kt
+++ app/src/main/java/tn/rifq_android/data/api/RetrofitInstance.kt
@@ -19,7 +19,7 @@ import tn.rifq_android.data.storage.TokenManager
 import java.util.concurrent.TimeUnit
 
 object RetrofitInstance {
-    private const val BASE_URL = "http://10.0.2.2:3000/"
+    private const val BASE_URL = "https://rifq.onrender.com/"
 
     private var tokenManager: TokenManager? = null
     
